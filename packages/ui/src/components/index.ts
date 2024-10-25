9a2daebaeb530660bac3b7498e68659fe1044c98
leonardo.goncalves <leogoncalves.contato@gmail.com>
2024-10-25 15:55:19 -0300

    🌱 export components

---
+++ packages/ui/src/components/index.ts
@@ -0,0 +1,12 @@
+export * from './Button';
+export * from './ButtonIcon';
+export * from './Card';
+export * from './Container';
+export * from './Form';
+export * from './Grid';
+export * from './Icon';
+export * from './Input';
+export * from './Ripple';
+export * from './Stack';
+export * from './Typography';
+
