94c3d02e51e4db3788b205f9f37faa49c59b78d6
leonardo.goncalves <leogoncalves.contato@gmail.com>
2024-10-25 15:54:02 -0300

    💄 rename icon component

--- packages/ui/src/components/Icon/Icon.tsx
+++ packages/ui/src/components/Icon/Icon.tsx
@@ -6,7 +6,7 @@ import type { Colors, Size } from '@/theme';
 import './Icons.scss';
 
 interface IconsProps extends HTMLAttributes<HTMLElement> { icon: string; size?: Size; color?: Colors; }
-export default function Icons({ icon, size = 'medium', color = 'primary', ...props }: IconsProps) {
+export default function Icon({ icon, size = 'medium', color = 'primary', ...props }: IconsProps) {
 
     const clss = joinClass(['cj-icon', 'uil', `uil-${icon}`, size, color, props.className]);
 
