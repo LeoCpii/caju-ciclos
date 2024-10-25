93a0c6769d41f1a23c54bed62488802741e52d37
leonardo.goncalves <leogoncalves.contato@gmail.com>
2024-10-25 00:34:40 -0300

    💄add useResize hook

---
+++ packages/ui/src/hooks/useResize/useResize.stories.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+import useResize from './useResize';
+
+const meta: Meta<typeof useResize> = {
+    title: 'hook/useResize'
+};
+
+export const Template: StoryObj<typeof useResize> = {
+    render: () => {
+        const [screen, setScreen] = useState('');
+        const [count, setCount] = useState(0);
+
+        useResize({
+            onMobile: () => { makeIt('mobile'); },
+            onTablet: () => { makeIt('tablet'); },
+            onDesktop: () => { makeIt('desktop'); },
+            onWidescreen: () => { makeIt('wide'); },
+            onFullHD: () => { makeIt('fullhd'); },
+        }, [count]);
+
+        const makeIt = (s: string) => {
+            setScreen(s);
+            setCount(count + 1);
+        };
+
+        return (
+            <div>
+                <h1>{screen}</h1>
+            </div>
+        );
+    }
+};
+
+export default meta;
---
+++ packages/ui/src/hooks/useResize/useResize.ts
@@ -0,0 +1,109 @@
+import { useEffect } from 'react';
+
+export enum DeviceBreakpoints {
+    MAX_MOBILE = '(max-width: 719px)',
+    MIN_TABLET = '(min-width: 720px)',
+    MAX_TABLET = '(max-width: 1023px)',
+    MIN_DESKTOP = '(min-width: 1024px)',
+    MAX_DESKTOP = '(max-width: 1279px)',
+    MIN_WIDESCREEN = '(min-width: 1280px)',
+    MAX_WIDESCREEN = '(max-width: 1439px)',
+    MIN_FULL_HD = '(min-width: 1440px)',
+}
+
+type Medias = {
+    mobile: MediaQueryList;
+    tablet: MediaQueryList;
+    desktop: MediaQueryList;
+    widescreen: MediaQueryList;
+    full_hd: MediaQueryList;
+}
+
+export interface Callback {
+    onMobile?: () => void;
+    onTablet?: () => void;
+    onFullHD?: () => void;
+    onDesktop?: () => void;
+    onWidescreen?: () => void;
+}
+
+const MEDIAS = {
+    mobile: DeviceBreakpoints.MAX_MOBILE,
+    tablet: `${DeviceBreakpoints.MIN_TABLET} and ${DeviceBreakpoints.MAX_TABLET}`,
+    desktop: `${DeviceBreakpoints.MIN_DESKTOP} and ${DeviceBreakpoints.MAX_DESKTOP}`,
+    widescreen: `${DeviceBreakpoints.MIN_WIDESCREEN} and ${DeviceBreakpoints.MAX_WIDESCREEN}`,
+    full_hd: DeviceBreakpoints.MIN_FULL_HD
+};
+
+const getMedias = (): Medias => {
+    return {
+        mobile: window.matchMedia(MEDIAS.mobile),
+        tablet: window.matchMedia(MEDIAS.tablet),
+        desktop: window.matchMedia(MEDIAS.desktop),
+        widescreen: window.matchMedia(MEDIAS.widescreen),
+        full_hd: window.matchMedia(MEDIAS.full_hd)
+    };
+};
+
+export default function useResize({
+    onMobile, onTablet, onDesktop, onWidescreen, onFullHD
+}: Callback, deps: any[] = []) {
+    const MAP_CALLBACKS = {
+        mobile: onMobile,
+        tablet: onTablet,
+        desktop: onDesktop,
+        widescreen: onWidescreen,
+        full_hd: onFullHD
+    };
+
+    const checker = (event: MediaQueryListEvent, fn: () => void) => { if (event.matches) { fn(); } };
+
+    const makeMobile = (event: MediaQueryListEvent) => {
+        if (onMobile) { checker(event, onMobile); }
+    };
+
+    const makeTablet = (event: MediaQueryListEvent) => {
+        if (onTablet) { checker(event, onTablet); }
+    };
+
+    const makeDesktop = (event: MediaQueryListEvent) => {
+        if (onDesktop) { checker(event, onDesktop); }
+    };
+
+    const makeWidescreen = (event: MediaQueryListEvent) => {
+        if (onWidescreen) { checker(event, onWidescreen); }
+    };
+
+    const makeFullhd = (event: MediaQueryListEvent) => {
+        if (onFullHD) { checker(event, onFullHD); }
+    };
+
+    const initialize = (medias: Medias) => {
+        const key = Object.keys(medias).find((key) => medias[key].matches) as keyof typeof MAP_CALLBACKS;
+        MAP_CALLBACKS[key]?.();
+    };
+
+    useEffect(() => {
+        const medias = getMedias();
+        initialize(medias);
+    }, []);
+
+    useEffect(() => {
+        const medias = getMedias();
+
+        onMobile && medias.mobile.addEventListener('change', makeMobile);
+        onTablet && medias.tablet.addEventListener('change', makeTablet);
+        onDesktop && medias.desktop.addEventListener('change', makeDesktop);
+        onWidescreen && medias.widescreen.addEventListener('change', makeWidescreen);
+        onFullHD && medias.full_hd.addEventListener('change', makeFullhd);
+
+        return () => {
+            onMobile && medias.mobile.removeEventListener('change', makeMobile);
+            onTablet && medias.tablet.removeEventListener('change', makeTablet);
+            onDesktop && medias.desktop.removeEventListener('change', makeDesktop);
+            onWidescreen && medias.widescreen.removeEventListener('change', makeWidescreen);
+            onFullHD && medias.full_hd.removeEventListener('change', makeFullhd);
+        };
+    }, deps);
+
+};
