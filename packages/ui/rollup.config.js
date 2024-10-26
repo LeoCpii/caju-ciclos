7a993646273ce823513673556a2756a3bd9964da
leonardo.goncalves <leogoncalves.contato@gmail.com>
2024-10-26 12:42:57 -0300

    👷 Improve ui build to map alias

--- packages/ui/rollup.config.js
+++ packages/ui/rollup.config.js
@@ -3,7 +3,6 @@ import { glob } from 'glob';
 import sass from 'rollup-plugin-sass';
 import postcss from 'rollup-plugin-postcss';
 import { terser } from 'rollup-plugin-terser';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 import image from '@rollup/plugin-image';
 import commonjs from '@rollup/plugin-commonjs';
@@ -11,30 +10,6 @@ import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 
 import packageJson from './package.json';
-
-const pluginsCSS = [
-    image(),
-    postcss({
-        use: [['sass']],
-        preprocessor: (_, id) => new Promise((resolve) => {
-            const result = sass.renderSync({ file: id });
-            resolve({ code: result.css.toString() });
-        }),
-        minimize: true,
-        sourceMap: true,
-        extract: 'styles/index.css',
-        extensions: ['.sass', '.css', '.scss'],
-        additionalData: '@use "src/styles/main.scss" as *;',
-    })
-];
-
-const pluginsTS = [
-    peerDepsExternal(),
-    resolve(),
-    commonjs(),
-    typescript({ tsconfig: './tsconfig.json', exclude: ['**/*.stories.tsx', 'dist'] }),
-    terser()
-];
 
 export default [
     {
@@ -52,6 +27,24 @@ export default [
                 preserveModulesRoot: 'src',
             },
         ],
-        plugins: [...pluginsTS, ...pluginsCSS],
+        plugins: [
+            commonjs(),
+            typescript({ tsconfig: './tsconfig.json' }),
+            resolve(),
+            terser(),
+            image(),
+            postcss({
+                use: [['sass']],
+                preprocessor: (_, id) => new Promise((resolve) => {
+                    const result = sass.renderSync({ file: id });
+                    resolve({ code: result.css.toString() });
+                }),
+                minimize: true,
+                sourceMap: true,
+                extract: 'styles/index.css',
+                extensions: ['.sass', '.css', '.scss'],
+                additionalData: '@use "src/styles/main.scss" as *;',
+            })
+        ],
     }
 ];
