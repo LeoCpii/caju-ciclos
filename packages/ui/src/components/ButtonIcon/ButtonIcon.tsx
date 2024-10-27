92b74822bc6b4f2aaad4a613899918c634828a08
leonardo.goncalves <leogoncalves.contato@gmail.com>
2024-10-27 15:25:33 -0300

    ✨ add custom colot to button icon

--- packages/ui/src/components/ButtonIcon/ButtonIcon.tsx
+++ packages/ui/src/components/ButtonIcon/ButtonIcon.tsx
@@ -2,18 +2,25 @@ import type { HTMLAttributes, PropsWithChildren } from 'react';
 
 import Ripple from '@/components/Ripple';
 import joinClass from '@/utils/joinClass';
+import { MappedColors, useTheme, convertPathToColor } from '@/theme';
 
 import './ButtonIcon.scss';
 
-type ButtonIconProps = PropsWithChildren<HTMLAttributes<HTMLButtonElement>>;
-export default function ButtonIcon({ children, ...props }: ButtonIconProps) {
+interface ButtonIconProps extends PropsWithChildren<HTMLAttributes<HTMLButtonElement>> {
+    color?: MappedColors;
+};
+export default function ButtonIcon({ children, color = 'primary.main', ...props }: ButtonIconProps) {
+    const { theme: { palette } } = useTheme();
+
     const cls = joinClass([
         'cj-button-icon',
         props.className
     ]);
 
+    const c = convertPathToColor(color, palette);
+
     return (
-        <button {...props} className={cls}>
+        <button {...props} className={cls} style={{ ...props.style, color: c }}>
             {children}
             <Ripple />
         </button>
