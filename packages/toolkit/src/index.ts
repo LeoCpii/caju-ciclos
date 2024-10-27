75849793b512238fc11a295eebefe51b5761dcbb
leonardo.goncalves <leogoncalves.contato@gmail.com>
2024-10-27 15:26:22 -0300

    🔨 add array helpers

--- packages/toolkit/src/index.ts
+++ packages/toolkit/src/index.ts
@@ -2,6 +2,7 @@ export * from './dom';
 export * from './jwt';
 export * from './url';
 export * from './uuid';
+export * from './array';
 export * from './logger';
 export * from './string';
 export * from './interface';
