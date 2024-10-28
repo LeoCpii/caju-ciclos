df97a39e0d0a1c3a7775d241af2514e9bc4986f2
leonardo.goncalves <leogoncalves.contato@gmail.com>
2024-10-28 13:41:30 -0300

    🔨 add useFilter hook

--- packages/ui/src/hooks/index.ts
+++ packages/ui/src/hooks/index.ts
@@ -1,3 +1,4 @@
 export { default as useResize } from './useResize';
+export { default as useFilter } from './useFilter';
 export { default as useOutsideClick } from './useOutsideClick';
 export { default as useListenerResized } from './useListenerResized';
